bee15d579e801a1db662ba27f9aed10be76d431d
Crowdbotics <team@crowdbotics.com>
2023-01-24 19:39:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kallon-f-38436",
+  url: "https://kallon-f-38436.botics.co",
+  api: "https://kallon-f-38436.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
